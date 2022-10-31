6685e7081eeff71b98804edbd5083814bb6a045e
kawtherkawa <kaouther.braiek@gmail.com>
2022-10-31 12:24:18 +0100

    this is a mini project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import Navigation from './componets/Navigation';
+import Home from './componets/Home';
+import Skills from './componets/Skills';
+import Contact from './componets/Contact';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <Home />
+      <Skills />
+      <Contact />
     </div>
   );
 }
